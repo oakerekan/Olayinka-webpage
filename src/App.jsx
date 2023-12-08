4fa25406e5ba27a5a2c16e2633a4c1f7fda7a4db
Olayinka Akerekan <105251441+oakerekan@users.noreply.github.com>
2023-12-08 05:43:36 +0000

    Olayinka Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Olayinka Akerekan",
+  title: "Data Analyst and Data Engineer",
+  email: "olayinkagaiusakerekan@gmail.com",
+  gitHub: "oakerekan",
+  instagram: "",
+  linkedIn: "olayinkaakerekan",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "_data_alchemist",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,21 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a data enigineering student studying at AltSchoolAfrica. I love working with data and I'm passionate about using data to solve problems. I'm currently learning how to use data to solve business problems.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "PostgreSQL",
+  "Microsoft Azure",
+  "Google Cloud Platform",
+  "Data Analysis",
+  "dbt",
+  "sklearn",
+  "pandas",
 ];
 
 /**
@@ -45,7 +47,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in creative ways in the data space. By leveraging my data experience I continually look for new and better ways to make tech accessible by all. I am interested in helping to improve your business profitability.";
 
 const About = () => {
   return (
